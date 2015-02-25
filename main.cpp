b79cc48782d0e84ea0fb29c602c8460dcf1decad
Giles Thomas <giles@giles.net>
2015-02-25 01:33:29 +0000

    Working through the docs... configure head-tracking

--- main.cpp
+++ main.cpp
@@ -6,22 +6,30 @@ int WINAPI WinMain(HINSTANCE hinst, HINSTANCE, LPSTR, int)
 {
 	ovr_Initialize();
 
-	ovrHmd HMD = ovrHmd_Create(0);
+	ovrHmd hmd = ovrHmd_Create(0);
 
-	if (!HMD)
+	if (!hmd)
 	{
-		MessageBoxA(NULL, "Oculus Rift not detected.", "", MB_OK); return(0); 
+		MessageBoxA(NULL, "Oculus Rift not detected.", "", MB_OK); 
+		return(0); 
 	}
 
-	if (HMD->ProductName[0] == '\0')  
+	if (hmd->ProductName[0] == '\0')  
 	{
 		MessageBoxA(NULL, "Rift detected, display not enabled.", "", MB_OK);
 	}
 
+	ovrHmd_ConfigureTracking(
+		hmd, 
+		ovrTrackingCap_Orientation | ovrTrackingCap_MagYawCorrection | ovrTrackingCap_Position, 
+		0
+	);
+
+
 	MessageBoxA(NULL, "Up and running, all initialized!", "", MB_OK);
 
 	// Release and close down
-	ovrHmd_Destroy(HMD);
+	ovrHmd_Destroy(hmd);
 	ovr_Shutdown();
 
 	return(0);
