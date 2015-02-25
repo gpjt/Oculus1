37ad8a33fe1e77fbb0afb610054a8e50de670274
Giles Thomas <giles@giles.net>
2015-02-25 01:23:12 +0000

    Copypasta from Tiny Room SDK demo -- enough to initialize the rift

--- main.cpp
+++ main.cpp
@@ -4,7 +4,25 @@
 
 int WINAPI WinMain(HINSTANCE hinst, HINSTANCE, LPSTR, int)
 {
-	// Initializes LibOVR, and the Rift
 	ovr_Initialize();
 
+	ovrHmd HMD = ovrHmd_Create(0);
+
+	if (!HMD)
+	{
+		MessageBoxA(NULL, "Oculus Rift not detected.", "", MB_OK); return(0); 
+	}
+
+	if (HMD->ProductName[0] == '\0')  
+	{
+		MessageBoxA(NULL, "Rift detected, display not enabled.", "", MB_OK);
+	}
+
+	MessageBoxA(NULL, "Up and running, all initialized!", "", MB_OK);
+
+	// Release and close down
+	ovrHmd_Destroy(HMD);
+	ovr_Shutdown();
+
+	return(0);
 }
